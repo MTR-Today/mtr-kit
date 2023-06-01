3c00183289eaeddbeb5c9e6bca22647229f6f0ec
marklai1998 <marklai1998@gmail.com>
2023-06-01 18:48:44 -0400

    feat: mtr api

---
+++ src/apis/stopScheduleApi.ts
@@ -0,0 +1,38 @@
+import { LineCode } from '../constants/line'
+import { StopCode } from '../constants/stop'
+import { apiClient } from './apiClient'
+
+export type ScheduleItem = {
+  seq: string
+  dest: string
+  plat: string
+  time: string
+  ttnt: string
+  valid: string
+  source: string
+}
+
+type Schedule = {
+  sys_time: string
+  curr_time: string
+  data: {
+    [key in `${LineCode}-${StopCode}`]: {
+      curr_time: string
+      sys_time: string
+      UP?: ScheduleItem[]
+      DOWN?: ScheduleItem[]
+    }
+  }
+  status: number
+  message: string
+  isdelay: string
+}
+
+const get = ({ line, stop }: { line: LineCode; stop: StopCode }) =>
+  apiClient
+    .url('/transport/mtr/getSchedule.php')
+    .query({ line, sta: stop })
+    .get()
+    .json<Schedule>()
+
+export const stopScheduleApi = { get }
