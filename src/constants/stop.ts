abd777851d704f0379e1bd7a2cdf70e0dc6c0bd5
marklai1998 <marklai1998@gmail.com>
2023-06-24 23:30:19 -0400

    feat: stop color

--- src/constants/stop.ts
+++ src/constants/stop.ts
@@ -103,588 +103,686 @@ export const TUM = {
   code: StopCode.TUM,
   nameEn: 'Tuen Mun',
   nameZh: '屯門',
+  color: '#035F94',
 }
 
 export const SIH = {
   code: StopCode.SIH,
   nameEn: 'Siu Hong',
   nameZh: '兆康',
+  color: '#79BFB3',
 }
 
 export const TIS = {
   code: StopCode.TIS,
   nameEn: 'Tin Shui Wai',
   nameZh: '天水圍',
+  color: '#FFCF2A',
 }
 
 export const LOP = {
   code: StopCode.LOP,
   nameEn: 'Long Ping',
   nameZh: '朗屏',
+  color: '#F697B4',
 }
 
 export const YUL = {
   code: StopCode.YUL,
   nameEn: 'Yuen Long',
   nameZh: '元朗',
+  color: '#42D7DC',
 }
 
 export const KSR = {
   code: StopCode.KSR,
   nameEn: 'Kam Sheung Road',
   nameZh: '錦上路',
+  color: '#BF431A',
 }
 
 export const TWW = {
   code: StopCode.TWW,
   nameEn: 'Tsuen Wan West',
   nameZh: '荃灣西',
+  color: '#A81C07',
 }
 
 export const MEF = {
   code: StopCode.MEF,
   nameEn: 'Mei Foo',
   nameZh: '美孚',
+  color: '#0088DD',
 }
 
 export const NAC = {
   code: StopCode.NAC,
   nameEn: 'Nam Cheong',
   nameZh: '南昌',
+  color: '#FFFF80',
 }
 
 export const AUS = {
   code: StopCode.AUS,
   nameEn: 'Austin',
   nameZh: '柯士甸',
+  color: '#D05A3A',
 }
 
 export const ETS = {
   code: StopCode.ETS,
   nameEn: 'East Tsim Sha Tsui',
   nameZh: '尖東',
+  color: '#ffff00',
 }
 
 export const HUH = {
   code: StopCode.HUH,
   nameEn: 'Hung Hom',
   nameZh: '紅磡',
+  color: '#FF2492',
 }
 
 export const HOM = {
   code: StopCode.HOM,
   nameEn: 'Ho Man Tin',
   nameZh: '何文田',
+  color: '#ABBB77',
 }
 
 export const TKW = {
   code: StopCode.TKW,
   nameEn: 'To Kwa Wan',
   nameZh: '土瓜灣',
+  color: '#95D0D0',
 }
 
 export const SUW = {
   code: StopCode.SUW,
   nameEn: 'Sung Wong Toi',
   nameZh: '宋皇臺',
+  color: '#E6C35C',
 }
 
 export const KAT = {
   code: StopCode.KAT,
   nameEn: 'Kai Tak',
   nameZh: '啟德',
+  color: '#EBC411',
 }
 
 export const DIH = {
   code: StopCode.DIH,
   nameEn: 'Diamond Hill',
   nameZh: '鑽石山',
+  color: '#202020',
 }
 
 export const HIK = {
   code: StopCode.HIK,
   nameEn: 'Hin Keng',
   nameZh: '顯徑',
+  color: '#72DC72',
 }
 
 export const TAW = {
   code: StopCode.TAW,
   nameEn: 'Tai Wai',
   nameZh: '大圍',
+  color: '#00008b',
 }
 
 export const CKT = {
   code: StopCode.CKT,
   nameEn: 'Che Kung Temple',
   nameZh: '車公廟',
+  color: '#EED9B8',
 }
 
 export const STW = {
   code: StopCode.STW,
   nameEn: 'Sha Tin Wai',
   nameZh: '沙田圍',
+  color: '#FFBCD9',
 }
 
 export const CIO = {
   code: StopCode.CIO,
   nameEn: 'City One',
   nameZh: '第一城',
+  color: '#FFCF2E',
 }
 
 export const SHM = {
   code: StopCode.SHM,
   nameEn: 'Shek Mun',
   nameZh: '石門',
+  color: '#FBEC5D',
 }
 
 export const TSH = {
   code: StopCode.TSH,
   nameEn: 'Tai Shui Hang',
   nameZh: '大水坑',
+  color: '#9DE4E0',
 }
 
 export const HEO = {
   code: StopCode.HEO,
   nameEn: 'Heng On',
   nameZh: '恆安',
+  color: '#B4CBE6',
 }
 
 export const MOS = {
   code: StopCode.MOS,
   nameEn: 'Ma On Shan',
   nameZh: '馬鞍山',
+  color: '#BAAADB',
 }
 
 export const WKS = {
   code: StopCode.WKS,
   nameEn: 'Wu Kai Sha',
   nameZh: '烏溪沙',
+  color: '#954535',
 }
 
 export const ADM = {
   code: StopCode.ADM,
   nameEn: 'Admiralty',
   nameZh: '金鐘',
+  color: '#',
 }
 
 export const EXC = {
   code: StopCode.EXC,
   nameEn: 'Exhibition Centre',
   nameZh: '會展',
+  color: '#94A8B0',
 }
 
 export const MKK = {
   code: StopCode.MKK,
   nameEn: 'Mong Kok East',
   nameZh: '旺角東',
+  color: '#006400',
 }
 
 export const KOT = {
   code: StopCode.KOT,
   nameEn: 'Kowloon Tong',
   nameZh: '九龍塘',
+  color: '#007FFF',
 }
 
 export const SHT = {
   code: StopCode.SHT,
   nameEn: 'Sha Tin',
   nameZh: '沙田',
+  color: '#BB7796',
 }
 
 export const FOT = {
   code: StopCode.FOT,
   nameEn: 'Fo Tan',
   nameZh: '火炭',
+  color: '#FFA500',
 }
 
 export const RAC = {
   code: StopCode.RAC,
   nameEn: 'Racecourse',
   nameZh: '馬場',
+  color: '#15AE69',
 }
 
 export const UNI = {
   code: StopCode.UNI,
   nameEn: 'University',
   nameZh: '大學',
+  color: '#A2D7DD',
 }
 
 export const TAP = {
   code: StopCode.TAP,
   nameEn: 'Tai Po Market',
   nameZh: '大埔墟',
+  color: '#976E9A',
 }
 
 export const TWO = {
   code: StopCode.TWO,
   nameEn: 'Tai Wo',
   nameZh: '太和',
+  color: '#C89F05',
 }
 
 export const FAN = {
   code: StopCode.FAN,
   nameEn: 'Fanling',
   nameZh: '粉嶺',
+  color: '#9ACD32',
 }
 
 export const SHS = {
   code: StopCode.SHS,
   nameEn: 'Sheung Shui',
   nameZh: '上水',
+  color: '#F6A600',
 }
 
 export const LOW = {
   code: StopCode.LOW,
   nameEn: 'Lo Wu',
   nameZh: '羅湖',
+  color: '#8DC476',
 }
 
 export const LMC = {
   code: StopCode.LMC,
   nameEn: 'Lok Ma Chau',
   nameZh: '落馬洲',
+  color: '#009E9B',
 }
 
 export const TSW = {
   code: StopCode.TSW,
   nameZh: '荃灣',
   nameEn: 'Tsuen Wan',
+  color: '#BB2200',
 }
 
 export const TWH = {
   code: StopCode.TWH,
   nameZh: '大窩口',
   nameEn: 'Tai Wo Hau',
+  color: '#A2B741',
 }
 
 export const KWH = {
   code: StopCode.KWH,
   nameZh: '葵興',
   nameEn: 'Kwai Hing',
+  color: '#F1CC00',
 }
 
 export const KWF = {
   code: StopCode.KWF,
   nameZh: '葵芳',
   nameEn: 'Kwai Fong',
+  color: '#233D3A',
 }
 
 export const LAK = {
   code: StopCode.LAK,
   nameZh: '茘景',
   nameEn: 'Lai King',
+  color: '#BB2200',
 }
 
 export const LCK = {
   code: StopCode.LCK,
   nameZh: '茘枝角',
   nameEn: 'Lai Chi Kok',
+  color: '#E04300',
 }
 
 export const CSW = {
   code: StopCode.CSW,
   nameZh: '長沙灣',
   nameEn: 'Cheung Sha Wan',
+  color: '#B5A265',
 }
 
 export const SSP = {
   code: StopCode.SSP,
   nameZh: '深水埗',
   nameEn: 'Sham Shui Po',
+  color: '#016258',
 }
 
 export const PRE = {
   code: StopCode.PRE,
   nameZh: '太子',
   nameEn: 'Prince Edward',
+  color: '#8674A1',
 }
 
 export const MOK = {
   code: StopCode.MOK,
   nameZh: '旺角',
   nameEn: 'Mong Kok',
+  color: '#BE2700',
 }
 
 export const YMT = {
   code: StopCode.YMT,
   nameZh: '油麻地',
   nameEn: 'Yau Ma Tei',
+  color: '#CCCCCC',
 }
 
 export const JOR = {
   code: StopCode.JOR,
   nameZh: '佐敦',
   nameEn: 'Jordan',
+  color: '#69B72B',
 }
 
 export const TST = {
   code: StopCode.TST,
   nameZh: '尖沙咀',
   nameEn: 'Tsim Sha Tsui',
+  color: '#FFEF00',
 }
 
 export const CEN = {
   code: StopCode.CEN,
   nameZh: '中環',
   nameEn: 'Central',
+  color: '#AA0000',
 }
 
 export const HOK = {
   code: StopCode.HOK,
   nameEn: 'Hong Kong',
   nameZh: '香港',
+  color: '#FFFAFA',
 }
 
 export const KOW = {
   code: StopCode.KOW,
   nameEn: 'Kowloon',
   nameZh: '九龍',
+  color: '#ACA28A',
 }
 
 export const OLY = {
   code: StopCode.OLY,
   nameEn: 'Olympic',
   nameZh: '奧運',
+  color: '#4584C4',
 }
 
 export const TSY = {
   code: StopCode.TSY,
   nameEn: 'Tsing Yi',
   nameZh: '青衣',
+  color: '#A1C6CA',
 }
 
 export const SUN = {
   code: StopCode.SUN,
   nameEn: 'Sunny Bay',
   nameZh: '欣澳',
+  color: '#808080',
 }
 
 export const TUC = {
   code: StopCode.TUC,
   nameEn: 'Tung Chung',
   nameZh: '	東涌',
+  color: '#6A5ACD',
 }
 
 export const NOP = {
   code: StopCode.NOP,
   nameEn: 'North Point',
   nameZh: '北角',
+  color: '#E86220',
 }
 
 export const QUB = {
   code: StopCode.QUB,
   nameEn: 'Quarry Bay',
   nameZh: '鰂魚涌',
+  color: '#00918C',
 }
 
 export const YAT = {
   code: StopCode.YAT,
   nameEn: 'Yau Tong',
   nameZh: '油塘',
+  color: '#FFEF00',
 }
 
 export const TIK = {
   code: StopCode.TIK,
   nameEn: 'Tiu Keng Leng',
   nameZh: '調景嶺',
+  color: '#DCD144',
 }
 
 export const TKO = {
   code: StopCode.TKO,
   nameEn: 'Tseung Kwan O',
   nameZh: '將軍澳',
+  color: '#E60012',
 }
 
 export const LHP = {
   code: StopCode.LHP,
   nameEn: 'LOHAS Park',
   nameZh: '康城',
+  color: '#826F79',
 }
 
 export const HAH = {
   code: StopCode.HAH,
   nameEn: 'Hang Hau',
   nameZh: '坑口',
+  color: '#2EA9DF',
 }
 
 export const POA = {
   code: StopCode.POA,
   nameEn: 'Po Lam',
-  nameZh: '	寶琳',
+  nameZh: '寶琳',
+  color: '#F28500',
 }
 
 export const AIR = {
   code: StopCode.AIR,
   nameEn: 'Airport',
   nameZh: '機場',
+  color: '#808080',
 }
 
 export const AWE = {
   code: StopCode.AWE,
   nameEn: 'AsiaWorld Expo',
   nameZh: '博覽館',
+  color: '#FFFFFF',
 }
 
 export const LAT = {
   code: StopCode.LAT,
   nameZh: '藍田',
   nameEn: 'Lam Tin',
+  color: '#0083BE',
 }
 
 export const KWT = {
   code: StopCode.KWT,
   nameZh: '觀塘',
   nameEn: 'Kwun Tong',
+  color: '#FFFFFF',
 }
 
 export const NTK = {
   code: StopCode.NTK,
   nameZh: '牛頭角',
   nameEn: 'Ngau Tau Kok',
+  color: '#92B6A3',
 }
 
 export const KOB = {
   code: StopCode.KOB,
   nameZh: '九龍灣',
   nameEn: 'Kowloon Bay',
+  color: '#C80815',
 }
 
 export const CHH = {
   code: StopCode.CHH,
   nameZh: '彩虹',
   nameEn: 'Choi Hung',
+  color: '#27408B',
 }
 
 export const WTS = {
   code: StopCode.WTS,
   nameZh: '黃大仙',
   nameEn: 'Wong Tai Sin',
+  color: '#FFFF00',
 }
 
 export const LOF = {
   code: StopCode.LOF,
   nameZh: '樂富',
   nameEn: 'Lok Fu',
+  color: '#579E2F',
 }
 
 export const SKM = {
   code: StopCode.SKM,
   nameZh: '石硤尾',
   nameEn: 'Shek Kip Mei',
+  color: '#669933',
 }
 
 export const WHA = {
   code: StopCode.WHA,
   nameZh: '黃埔',
   nameEn: 'Whampo',
+  color: '#AECFF0',
 }
 
 export const DIS = {
   code: StopCode.DIS,
   nameZh: '迪士尼',
   nameEn: 'Disneyland Resort',
+  color: '#005533',
 }
 
 export const CHW = {
   code: StopCode.CHW,
   nameZh: '柴灣',
   nameEn: 'Chai Wan',
+  color: '#38510E',
 }
 
 export const HFC = {
   code: StopCode.HFC,
   nameZh: '杏花邨',
   nameEn: 'Heng Fa Chuen',
+  color: '#C01204',
 }
 
 export const SKW = {
   code: StopCode.SKW,
   nameZh: '筲箕灣',
   nameEn: 'Shau Kei Wan',
+  color: '#191970',
 }
 
 export const SWH = {
   code: StopCode.SWH,
   nameZh: '西灣河',
   nameEn: 'Sai Wan Ho',
+  color: '#FFCC00',
 }
 
 export const TAK = {
   code: StopCode.TAK,
   nameZh: '太古',
   nameEn: 'Tai Koo',
+  color: '#BB2200',
 }
 
 export const FOH = {
   code: StopCode.FOH,
   nameZh: '炮台山',
   nameEn: 'Fortress Hill',
+  color: '#4B8842',
 }
 
 export const TIH = {
   code: StopCode.TIH,
   nameZh: '天后',
   nameEn: 'Tin Hau',
+  color: '#FF7D00',
 }
 
 export const CAB = {
   code: StopCode.CAB,
   nameZh: '銅鑼灣',
   nameEn: 'Causeway Bay',
+  color: '#C8A2C8',
 }
 
 export const WAC = {
   code: StopCode.WAC,
   nameZh: '灣仔',
   nameEn: 'Wan Chai',
+  color: '#E1EB2B',
 }
 
 export const SHW = {
   code: StopCode.SHW,
   nameZh: '上環',
   nameEn: 'Sheung Wan',
+  color: '#FFD280',
 }
 
 export const SYP = {
   code: StopCode.SYP,
   nameZh: '西營盤',
   nameEn: 'Sai Ying Pun',
+  color: '#8B7BA0',
 }
 
 export const HKU = {
   code: StopCode.HKU,
   nameZh: '香港大學',
   nameEn: 'HKU',
+  color: '#B8DA89',
 }
 
 export const KET = {
   code: StopCode.KET,
   nameZh: '堅尼地城',
   nameEn: 'Kennedy Town',
+  color: '#95D0D0',
 }
 
 export const OCP = {
   code: StopCode.OCP,
   nameZh: '海洋公園',
   nameEn: 'Ocean Park',
+  color: '#00BFFF',
 }
 
 export const WCH = {
   code: StopCode.WCH,
   nameZh: '黃竹坑',
   nameEn: 'Wong Chuk Hang',
+  color: '#FFFF00',
 }
 
 export const LET = {
   code: StopCode.LET,
   nameZh: '利東',
   nameEn: 'Lei Tung',
+  color: '#FF7F00',
 }
 
 export const SOH = {
   code: StopCode.SOH,
   nameZh: '海怡半島',
   nameEn: 'South Horizons',
+  color: '#74B11B',
 }
 
 export const stops = [
