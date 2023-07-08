728a2296b0935ec57db9623dd4c0bc784c7e7358
marklai1998 <marklai1998@gmail.com>
2023-07-07 20:37:40 -0400

    fix: csv parse

--- src/utils/parseCsv.ts
+++ src/utils/parseCsv.ts
@@ -3,6 +3,7 @@ import Papa from 'papaparse'
 export const parseCsv = <T>(input: string) =>
   Papa.parse<T>(input, {
     header: true,
+    skipEmptyLines: true,
     transform: value => {
       const casted = Number(value)
       return isNaN(casted) ? value : casted
